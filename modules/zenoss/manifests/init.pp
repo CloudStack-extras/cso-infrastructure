fa07872c46fdd1946135dc089688bc26a9119420
David Nalley <david@gnsa.us>
2011-08-04 18:30:43 -0400

    adding zenoss manifest

---
+++ modules/zenoss/manifests/init.pp
@@ -0,0 +1,18 @@
+class zenoss { 
+	yumrepo { "zenoss":
+		baseurl => "http://dev.zenoss.org/yum/stable/",
+		name => "Zenoss",
+		enabled => 1,
+		gpgcheck => 0,
+	}
+
+	package { zenoss:
+		ensure => "3.1.0",
+		}
+
+	package { zenoss-core-zenpacks:
+		ensure => "3.1.0",
+	}
+
+}
+
