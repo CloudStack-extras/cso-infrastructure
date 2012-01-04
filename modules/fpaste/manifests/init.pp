68f7fa60a8da5dfa69c7d78db0933699339e205f
David Nalley <david@gnsa.us>
2012-01-03 19:38:00 -0500

    more fpaste changes

--- modules/fpaste/manifests/init.pp
+++ modules/fpaste/manifests/init.pp
@@ -4,12 +4,20 @@ class fpaste {
   package { mod_wsgi: ensure => present }
   package { python-psycopg2: ensure => present }
   
-   file { "/usr/lib/python2.7/site-packages/fpaste_server/static/images/favicon.ico":
+  file { "/usr/lib/python2.7/site-packages/fpaste_server/static/images/favicon.ico":
                 owner   => root,
                 group   => root,
                 mode    => 644,
                 source  => "puppet://puppet/fpaste/favicon.ico",
         }
+
+  file { "/usr/lib/python2.7/site-packages/fpaste_server/static/images/banner.png":
+                owner   => root,
+                group   => root,
+                mode    => 644,
+                source  => "puppet://puppet/fpaste/banner.png",
+        }
+
 
 file { "/usr/lib/python2.7/site-packages/fpaste_server/templates/fpaste_server/base.html":
   owner => root, 
