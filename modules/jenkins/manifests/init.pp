61c73cab706fd37e2b8faee593639f364b407b12
David Nalley <david@gnsa.us>
2012-01-20 03:49:43 -0500

    adding the jenkins repo

---
+++ modules/jenkins/manifests/init.pp
@@ -0,0 +1,21 @@
+class jenkins { 
+
+  yumrepo { jenkins: 
+    baseurl => "http://pkg.jenkins-ci.org/redhat/jenkins.repo",
+    enabled => true,
+    name => jenkins,
+    gpgcheck => 0,
+  }
+
+  package { jenkins:
+    requires => Yumrepo[jenkins],
+    ensure => present,
+  }
+
+  service { jenkins: 
+    requires => Package[jenkins],
+    enable => true,
+    hasstatus => true,
+  }
+
+}
