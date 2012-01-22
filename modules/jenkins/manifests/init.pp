25bfb42ff0098338ad6151aa0f5471fb44af2c02
David Nalley <david@gnsa.us>
2012-01-22 13:44:05 -0500

    adding git checking stuff to jenkins

--- modules/jenkins/manifests/init.pp
+++ modules/jenkins/manifests/init.pp
@@ -7,23 +7,30 @@ class jenkins {
     gpgcheck => 0,
   }
 
-  package { jenkins:
-    ensure => present,
-  }
+  package { jenkins: ensure => present }
   package { java-1.7.0-openjdk: ensure => present}
   package { dejavu-lgc-sans-fonts: ensure => present}
   package { dejavu-lgc-sans-mono-fonts: ensure => present}
   package { tomcat6: ensure => present}
+  package { git: ensure => latest}
+
   service { jenkins: 
     require => Package[jenkins],
     enable => true,
     hasstatus => true,
     ensure => true,
   }
+
   firewall { '8080 allow http-alt':
     proto       => 'tcp',
     dport       => '8080',
     action        => 'accept',
+  }
+
+  cron { commit_changes_to_SCM:
+    command => "cd /var/lib/jenkins && git add . && git commit -a -m 'routine update' && git push origin master",
+    user => jenkins,
+    minute  => '*/30',
   }
 
 }
