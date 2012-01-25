db401fcb5b493619b0cf968a3c167a05592b3f62
David Nalley <david@gnsa.us>
2012-01-24 20:40:16 -0500

    changing mkisofs to genisoimg

--- modules/jenkins/manifests/builder.pp
+++ modules/jenkins/manifests/builder.pp
@@ -3,5 +3,5 @@ class jenkins::builder {
   package { ant: ensure => present }
   package { log4j: ensure => present }
   package { cglib: ensure => present }
-  package { mkisofs: ensure => present } 
+  package { genisoimg: ensure => present } 
 }
