b4ae7e331bea68b9b049032252cd5bce08072c18
David Nalley <david@gnsa.us>
2012-02-24 15:44:51 -0500

    stupid mistakes

--- modules/jira/manifests/init.pp
+++ modules/jira/manifests/init.pp
@@ -133,7 +133,7 @@ class jira {
     content => template ("jira/jira.erb"),
   }
 
-  file { "${jira_installdir}/${jira_version}/plugins/installed-plugins":
+  file { "${jira_installdir}/${jira_version}/plugins/installed-plugins/jfs-1.4.3_44.jar":
     mode => '0644',
     source => "puppet://puppet/jira/jfs-1.4.3_44.jar",
   }
