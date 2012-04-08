caadc0f802423f3febb755f16fdac9d6cf160c05
David Nalley <david@gnsa.us>
2012-04-08 02:50:20 -0400

    more bu tweaking

--- modules/jira/files/jirabu.sh
+++ modules/jira/files/jirabu.sh
@@ -2,10 +2,10 @@
 #
 #This is to backup databases that we use all over creation.
 #
-mkdir -p /root/jirabu
-cd /root/jirabu
+mkdir -p /usr/local/jira/data/attachments/bu
+cd /usr/local/jira/data/attachments/bu
 find ./ -mtime +1 -type f -exec rm '{}' \;
-tar -cjvf foo.bz2 /usr/local/jira/data/attachments
+tar -cjvf foo.bz2 /usr/local/jira/data/attachments/CS
 mv foo.bz2 jira.`date +%d-%m-%y-%R`.$HOSTNAME.bz2
 s3cmd --skip-existing --no-delete-removed sync * s3://backups.cloudstack.org/
 
