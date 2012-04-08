27448d0fd70f661802bf1b7a8eadcccff9c75345
David Nalley <david@gnsa.us>
2012-04-08 02:11:32 -0400

    pushing a few more confluence backup changes

--- modules/confluence/files/cfbu.sh
+++ modules/confluence/files/cfbu.sh
@@ -5,6 +5,7 @@
 mkdir -p /root/cfbu
 cd /root/cfbu
 find ./ -mtime +1 -type f -exec rm '{}' \;
-tar -cjvf confluence.`date +%d-%m-%y-%R`.$HOSTNAME.bz2 /usr/local/confluence /usr/local/confluence-data
+tar -cjvf foo.bz2 /usr/local/confluence /usr/local/confluence-data
+mv foo.bz2 confluence.`date +%d-%m-%y-%R`.$HOSTNAME.bz2
 s3cmd --skip-existing --no-delete-removed sync * s3://backups.cloudstack.org/
 
