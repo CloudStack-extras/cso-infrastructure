932d3fcffcc577d0054c1604ce865a7de3f27420
David Nalley <david@gnsa.us>
2012-04-08 02:28:39 -0400

    adding jira backup file

---
+++ modules/jira/files/jirabu.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+#This is to backup databases that we use all over creation.
+#
+mkdir -p /root/jirabu
+cd /root/jirabu
+find ./ -mtime +1 -type f -exec rm '{}' \;
+tar -cjvf foo.bz2 /usr/local/confluence /usr/local/confluence-data
+mv foo.bz2 jira.`date +%d-%m-%y-%R`.$HOSTNAME.bz2
+s3cmd --skip-existing --no-delete-removed sync * s3://backups.cloudstack.org/
+
