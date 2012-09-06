021220bc86f03a3276e4397d7c13ae00d1f13285
David Nalley <david@gnsa.us>
2012-09-06 18:10:14 -0400

    adding more dependencies for the build

--- modules/jenkins/manifests/builder.pp
+++ modules/jenkins/manifests/builder.pp
@@ -6,6 +6,10 @@ class jenkins::builder {
   package { genisoimage: ensure => present } 
   package { checkstyle: ensure => present } 
   package { mysql-server: ensure => present}
+  package { maven: ensure => present } 
+  package { gcc: ensure => present }
+  package { glibc-devel: ensure => present }
+  package { MySQL-python: ensure => present } 
   service { mysqld:
     name => $operatingsystem ? {
       default => "mysqld",
