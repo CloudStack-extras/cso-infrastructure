c0a64a36188dae2febc481e5d3bedf9a0400bde7
Prasanna Santhanam <tsp@apache.org>
2013-11-14 15:28:01 +0530

    initial configuration of rpmbuilder
    Signed-off-by: Prasanna Santhanam <tsp@apache.org>

---
+++ modules/rpmbuilder/manifests/init.pp
@@ -0,0 +1,27 @@
+#rpmbuilder - build tools mvn, java, createrepo, yum
+
+class rpmbuilder {
+  include maven
+
+  $linux_packages = ['wget', 'curl', 'openssh-clients', 'mysql-server', 'gcc', 'glibc-devel']
+  $java_packages = ['jakarta-commons-collections', 'tomcat', 'java-1.6.0-openjdk-devel'] 
+  package { $packages: 
+      ensure => installed,
+  }
+
+  #Needed for systemvm.iso
+  package { 'genisoimage':
+    ensure => installed,
+  }
+
+  case $operatingsystem {
+    centos,redhat : {
+    }
+    fedora : {
+    }
+  }
+
+  service { 'mysqld':
+    ensure => running,
+  }
+}
